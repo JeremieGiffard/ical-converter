cfc25696e77136c46456c51175ec302703c6532d
jeremie giffard <e132784f@gmail.com>
2023-10-19 22:52:46 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	b, err := os.ReadFile("callendar-example.ics")
+	if err != nil {
+		fmt.Println(err)
+	}
+	str := string(b)
+	fmt.Println("str", str)
+	sliceStr := strings.Split(strings.ReplaceAll(str, "\r\n", "\n"), "\n")
+	fmt.Println("\n\n\n\nslice converted", sliceStr)
+	isCallendarValid(sliceStr)
+
+}
+
+func isCallendarValid(sliceStr []string) bool {
+	var isValid = false
+	fmt.Println("\n isCallendarValid()", " sliceStr[0] =", sliceStr[0], " sliceStr[last] =", sliceStr[len(sliceStr)-1])
+	if sliceStr[0] == "BEGIN:VCALENDAR" && sliceStr[len(sliceStr)-1] == "END:VCALENDAR" {
+		isValid = true
+	}
+
+	return isValid
+}
