c40de9d52ac177d40529221bf5bb927159db4b0a
jeremie giffard <e132784f@gmail.com>
2023-10-22 21:09:04 +0200

    filterColumns method to take only key we need

--- main.go
+++ main.go
@@ -11,8 +11,18 @@ func main() {
 	sliceStr, _ := readFileAndSlice("callendar-example.ics")
 	isCallendarValid(sliceStr)
 
+	SliceColumn := []string{"dtstart", "dtend", "description", "location", "summary"}
+
+	CsvMapFromIcs := makeCsvMapFromIcs(sliceStr)
+	// fmt.Println(CsvMapFromIcs)
+	ColumnsNames, ValuesToColumns := filterColumn(CsvMapFromIcs, SliceColumn)
+
 	csvData := [][]string{
-		{"dtstart", "dtend", "description", "location", "summary"}}
+		ColumnsNames, ValuesToColumns}
+
+	// for _, value := range ValuesToColumns {
+	// 	csvData = append(csvData, []string{value})
+	// }
 
 	file, e := os.Create("./callendar-converted.csv")
 	if e != nil {
@@ -26,6 +36,21 @@ func main() {
 
 }
 
+func filterColumn(mapStr map[string]string, ColumnsWanted []string) ([]string, []string) {
+	keysToReturn := []string{}
+	valuesToReturn := []string{}
+	for _, key := range ColumnsWanted {
+		value, keyExist := mapStr[strings.ToUpper(key)]
+		if keyExist {
+			keysToReturn = append(keysToReturn, key)
+			valuesToReturn = append(valuesToReturn, value)
+		}
+		fmt.Println("After filter key ", keysToReturn)
+		fmt.Println("After filter value ", valuesToReturn)
+	}
+	return keysToReturn, valuesToReturn
+}
+
 func readFileAndSlice(filePath string) ([]string, error) {
 	b, err := os.ReadFile(filePath)
 	if err != nil {
@@ -33,7 +58,7 @@ func readFileAndSlice(filePath string) ([]string, error) {
 	}
 	str := string(b)
 	sliceStr := strings.Split(strings.ReplaceAll(str, "\r\n", "\n"), "\n")
-	fmt.Println("\n\n\n\nslice converted", sliceStr)
+	//fmt.Println("\n\n\n\nslice converted", sliceStr)
 	return sliceStr, err
 }
 
@@ -45,4 +70,20 @@ func isCallendarValid(sliceStr []string) bool {
 	}
 
 	return isValid
+}
+
+func makeCsvMapFromIcs(sliceStr []string) map[string]string {
+	var csvDataTable = make(map[string]string)
+	for _, v := range sliceStr {
+		separator := ":"
+		if strings.Contains(v, "DTSTART;") || strings.Contains(v, "DTEND;") || strings.Contains(v, "ATTACH;") {
+			separator = ";"
+		}
+
+		SliceKeyAndValue := strings.SplitN(v, separator, 2)
+		csvDataTable[SliceKeyAndValue[0]] = SliceKeyAndValue[1]
+
+	}
+	fmt.Printf("%q\n", csvDataTable)
+	return csvDataTable
 }
